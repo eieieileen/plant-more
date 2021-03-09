589ffda3423d292cea752347987b9a3c3b9792c1
Eileen Lassche <eileenlassche@gmail.com>
2021-03-09 17:44:02 +0100

    part 2 finished

--- client/src/login.js
+++ client/src/login.js
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "./axios";
 import { Link } from "react-router-dom";
 
+
+
 export default class Login extends React.Component {
     constructor() {
         super();
@@ -10,11 +12,37 @@ export default class Login extends React.Component {
         };
     }
 
+    handleClick() {
+        console.log("clicked!!!!!!");
+        axios.post("/login", this.state).then(({data}) => {
+            if (data.success) {
+                location.replace("/");
+            } else {
+                this.setState({
+                    error: true,
+                });
+            }
+        });
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        },
+        () => console.log("this.state after set.state", this.state)
+        
+        );
+    }
+
     render() {
         return (
             <div id="loginDiv">
                 <h1> LOG IN </h1>
-                <input name="email" placeholder="email" />
+                <input name="email" placeholder="email" onChange={(e) => this.handleChange(e)} />
+                <input name="password" placeholder="password" type="password" onChange={(e) => this.handleChange(e)} />
+                <button onClick={() => this.handleClick()}>SUBMIT</button>
+
+                <Link to={"/"}>CLICK HERE TO REGISTER</Link> 
             </div>
         );
     }
